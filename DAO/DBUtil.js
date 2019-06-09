1ec96d42ead4fe67cd68833ca140fe38c318cccc
yu <yyc102218>
2019-06-09 15:39:50 +0800

    上线、访问地址修改

--- DAO/DBUtil.js
+++ DAO/DBUtil.js
@@ -2,7 +2,8 @@ var mysql = require("mysql");
 
 function createConnection() {
   var connection = mysql.createConnection({
-    host: "127.0.0.1",
+    // host: "127.0.0.1",
+    host: "192.168.1.42",
     port: "3306",
     user: "root",
     password: "yyc102218",
